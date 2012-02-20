d52b1bab35b2513df9dec2b7eb5f6352387c45a7
zippko <zvak.martin@gmail.com>
2012-02-20 20:45:06 +0100

    test

--- js/jquery.admin.js
+++ js/jquery.admin.js
@@ -1,5 +1,5 @@
 // default (site settings)
-$("#frmeditForm-headerImage-0").live("click",function () {$("#headerImageId").hide(1004);}); 
+$("#frmeditForm-headerImage-0").live("click",function () {$("#headerImageId").hide(1005);}); 
 $("#frmeditForm-headerImage-1").live("click",function () {$("#headerImageId").fadeIn(1000);});      
 $("#frmeditForm-colourScheme-0").live("click",function () {$("#colourSchemeId").hide(1000);});       
 $("#frmeditForm-colourScheme-1").live("click",function () {$("#colourSchemeId").fadeIn(1000);});  
